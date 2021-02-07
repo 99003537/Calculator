0118ae6a81a0c3d62a7943204044c9ec1cb83b13
99003532 <namratha.s@ltts.com>
2021-02-07 16:48:27 +0530

    Update test_functions.c

--- Implementation/Test/test_functions.c
+++ Implementation/Test/test_functions.c
@@ -70,6 +70,8 @@ void test_multiplication(void)
     TEST_ASSERT_EQUAL(6, multiplication(2,3)); 
     //LL_03_01
     TEST_ASSERT_EQUAL(5.0, multiplication(1.0,5.0)); 
+    //LL_03_03
+    TEST_ASSERT_EQUAL(12.0, multiplication(2.0,6.0));
 }
 
 void test_divide(void){
