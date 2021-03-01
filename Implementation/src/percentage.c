dd04dde7594b01bb90b74f808234da9da74e2788
99003529 <78536051+99003529@users.noreply.github.com>
2021-03-01 17:11:40 +0530

    Update percentage.c

--- Implementation/src/percentage.c
+++ Implementation/src/percentage.c
@@ -3,6 +3,7 @@
 
 float percentage(float num1, float num2)
   
+/*
 {
    float total;
    if(num2==0)
@@ -18,4 +19,21 @@ float percentage(float num1, float num2)
    //return total;
    }
 }
-
+*/
+  
+{
+    float percent;
+   
+    scanf("%f %f", &num1,&num2);
+    if(num1==0 || num2==0)
+    {
+        printf("Both the numbers entered should be greater than 0");
+    }
+    else
+    {
+       
+    percent = (num1 *num2 )/100;
+    
+    }
+    return percent;
+}
