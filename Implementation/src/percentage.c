184d7fc530568e6d51c99c44cdd2779073643f38
stepin104974 <69135861+stepin-104974-sumit@users.noreply.github.com>
2021-03-01 17:26:03 +0530

    s

--- Implementation/src/percentage.c
+++ Implementation/src/percentage.c
@@ -28,17 +28,18 @@ float percentage(float num1, float num2)
 {
     float percent;
    
-    scanf("%f %f", &num1,&num2);
-    if(num1==0 || num2==0)
+    //scanf("%f %f", &num1,&num2);
+    if(num2==0)
     {
-        printf("Both the numbers entered should be greater than 0");
+        printf("denominator enterede should be greater than 0");
+        percent=0;
     }
     else
     {
        
-    percent = (num1 *num2 )/100;
+    percent = (num1 /num2 )*100;
     
     }
     return percent;
 }
->>>>>>> dd04dde7594b01bb90b74f808234da9da74e2788
+
